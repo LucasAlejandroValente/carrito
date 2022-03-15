fe6eb0425852db0995f3d881c2761b4f414af255
Lucas Valente <lucasvalente0904@gmail.com>
2022-03-14 23:41:22 -0300

    add swal

---
+++ js/sweetAlert.js
@@ -0,0 +1,11 @@
+function showAlert(){Swal.fire({
+    position: 'bottom-start',
+    icon: 'info',
+    title: '<p class="swal-text">Has añadido un producto al carrito.</p>',
+    showConfirmButton: false,
+    timer: 1500,
+    allowOutsideClick: true,
+    allowEscapeKey: true,
+    allowEnterKey: true,
+    })
+};
